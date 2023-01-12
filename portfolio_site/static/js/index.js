c6c39840f787f239c32b2442103dfab396be8cc8
Matthew Summers <imadethisforcoding@gmail.com>
2023-01-13 06:31:05 +0800

    added individual project pages

--- portfolio_site/static/js/index.js
+++ portfolio_site/static/js/index.js
@@ -2,6 +2,7 @@ var menu_button = document.getElementById("menu-button");
 var close_button = document.getElementById("close");
 var modal = document.querySelector(".modal-menu");
 
+
 menu_button.addEventListener("click", function(){
     modal.style.display = "block";
 });
--- portfolio_site/views.py
+++ portfolio_site/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render
 
 def home(request):
     context = {
-        "home":"active"
+        "home":"active",
+        "title": "Portfolio"
     }
     return render(request, "home.html", context)
 
@@ -12,16 +13,33 @@ def projects(request):
     context = {
         "projects":"active"
     }
+
+    if "title" in request.GET:
+        context["single"] = True
+        if request.GET["title"] == "Ticket-Easy":
+            context["title"] = "Ticket Easy"
+            context["project"] = "ticket-easy"
+        elif request.GET["title"] == "Stream-Bunny":
+            context["title"] = "Stream Bunny"
+            context["project"] = "stream-bunny"
+        elif request.GET["title"] == "session-tracker":
+            context["title"] = "Session Tracker"
+            context["project"] = "session-tracker"
+    else:
+        context["title"] = "Projects"
+
     return render(request, "projects.html", context)
 
 def resume(request):
     context = {
-        "resume":"active"
+        "resume":"active",
+        "title": "Resume"
     }
     return render(request, "resume.html", context)
 
 def contact(request):
     context = {
-        "contact":"active"
+        "contact":"active",
+        "title": "Contact"
     }
     return render(request, "contact.html", context)
