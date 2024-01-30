e2333cf5433d4e74bc98006b0f17be5b38d830ec
Matthew Summers <imadethisforgithub@gmail.com>
2024-01-30 16:57:32 +0800

    updated projects overview, resume, nav

--- portfolio_site/static/js/faders.js
+++ portfolio_site/static/js/faders.js
@@ -1,26 +1,43 @@
-// const faders = document.querySelectorAll(".fade-in")
 const sliders = document.querySelectorAll(".slide-in")
 
-const appearOptions = {
-    threshold: 0,
-    rootMargin: "0px 0px -250px 0px"
-};
-const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll){
-    entries.forEach(entry => {
-        if (!entry.isIntersecting){
-            return
-        } else {
-            entry.target.classList.add("appear");
-            appearOnScroll.unobserve(entry.target);
-        }
+function slideInPojectDesc(){
+// const faders = document.querySelectorAll(".fade-in")
+
+    const appearOptions = {
+        threshold: 0,
+        rootMargin: "0px 0px -250px 0px"
+    };
+
+    const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll){
+        entries.forEach(entry => {
+            if (!entry.isIntersecting){
+                return
+            } else {
+                entry.target.classList.add("appear");
+                appearOnScroll.unobserve(entry.target);
+            }
+        });
+    }, appearOptions);
+
+    // faders.forEach(fader => {
+    //     appearOnScroll.observe(fader);
+    // });
+    sliders.forEach(slider =>{
+        appearOnScroll.observe(slider);
     });
-}, appearOptions);
+}
 
-// faders.forEach(fader => {
-//     appearOnScroll.observe(fader);
-// });
+function isMobileDevice() {
+    return /iPhone|iPad|iPod|Android|webOS|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
 
-
-sliders.forEach(slider =>{
-    appearOnScroll.observe(slider);
-});
+if (!isMobileDevice()) {
+    slideInPojectDesc();
+}else{
+    sliders.forEach(slider => {
+        slider.classList.remove("slide-in");
+        slider.classList.remove("left");
+        slider.classList.remove("right");
+        console.log(slider)
+    });
+}
